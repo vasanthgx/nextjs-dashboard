6bf9cd696c351e1120a776b3c84c24d1b44779d0
Vasanth Gopal <vasanth1627@gmail.com>
2024-08-14 23:14:50 +0530

    Implemented server side form validation using useActionState

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -10,7 +10,8 @@ import {
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
 import { UpdateInvoice } from './buttons';
-import { updateInvoice } from '@/app/lib/actions';
+import { updateInvoice, State } from '@/app/lib/actions';
+import { useActionState } from 'react';
 
 
 export default function EditInvoiceForm({
@@ -21,9 +22,11 @@ export default function EditInvoiceForm({
   customers: CustomerField[];
 }) {
 
-    const updateInvoiceWithId = updateInvoice.bind(null, invoice.id );
+      const initialState: State = { message: null, errors: {} };
+      const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+      const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
   return (
-    <form action={updateInvoiceWithId}>
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
